bfd456cf4dba7e9fc146e8be98b36d4ff7717b4f
Przemyslaw Koroscik <przemyslaw.koroscik@gmail.com>
2020-05-13 15:08:23 +0200

    Imported about component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-  import AboutPage from "./pages/about/About.svelte";
   import ContactPage from "./pages/contact/Contact.svelte";
   import CvPage from "./pages/cv/cv.svelte";
   import Navigation from "./navigation/Navigation.svelte";
   import PortfolioPage from "./pages/portfolio/Portfolio.svelte";
   import StartPage from "./pages/start/Start.svelte";
+  import AboutPage from "./pages/about/About.svelte";
 
   let renderComponent = StartPage;
 </script>
