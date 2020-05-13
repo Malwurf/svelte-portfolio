0d8668a3f9dc3ca5ffaaa4bf36e8571d25366ebf
Przemyslaw Koroscik <przemyslaw.koroscik@gmail.com>
2020-05-13 15:21:17 +0200

    Renamed components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
+  import AboutPage from "./pages/about/About.svelte";
   import ContactPage from "./pages/contact/Contact.svelte";
   import Navigation from "./navigation/Navigation.svelte";
   import PortfolioPage from "./pages/portfolio/Portfolio.svelte";
   import StartPage from "./pages/start/Start.svelte";
-  import AboutPage from "./pages/about/About.svelte";
 
   let renderComponent = StartPage;
 </script>
